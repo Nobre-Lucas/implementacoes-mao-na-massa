d3861e82e44f32bf486c1944f5ef8230b65a9cac
Lucas Nobre Barbosa <lucasbnobre18@gmail.com>
2025-12-17 09:22:35 -0300

    feat(bow): desenvolvida primeira versão de um bag-of-words primitivo

--- inteligencia_artificial/processamento_de_linguagem_natural/vetorizacao/python/bag_of_words.py
+++ inteligencia_artificial/processamento_de_linguagem_natural/vetorizacao/python/bag_of_words.py
@@ -1,6 +1,8 @@
+import string
+
 def conta_ocorrencias(palavra_escolhida: str, sentenca: str) -> int:
     contador = 0
-    for palavra in sentenca:
+    for palavra in sentenca.split():
         if palavra == palavra_escolhida:
             contador += 1
 
@@ -8,6 +10,12 @@ def conta_ocorrencias(palavra_escolhida: str, sentenca: str) -> int:
 
 if __name__ == "__main__":
     sentenca = input('Digite uma frase: ')
+    sentenca = sentenca.translate(str.maketrans('', '', string.punctuation))
+    sentenca = sentenca.lower()
     palavras = sentenca.split()
+    bow = {}
     for palavra in palavras:
-        print(f"{palavra}: {conta_ocorrencias(palavra, sentenca)}")
+        bow[palavra] = conta_ocorrencias(palavra, sentenca)
+
+    print(bow)
+        
